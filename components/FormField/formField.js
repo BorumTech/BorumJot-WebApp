d93b58e24904e28e171c0737e4cbf9814ef3ccc6
Varun <31084854+Borumer@users.noreply.github.com>
2020-10-27 19:16:29 -0400

    Make successful login request

--- components/FormField/formField.js
+++ components/FormField/formField.js
@@ -1,8 +1,8 @@
-export default function FormField({autofocus="false", label, format="text"}) {
+export default function FormField({autofocus=false, label, format="text",required=false}) {
     return (
         <fieldset>
-            <legend><label for={label}>{label[0].toUpperCase() + label.substring(1).toLowerCase()}</label></legend>
-            <input autofocus={autofocus} type={format} id={label} name={label} />
+            <legend><label htmlFor={label}>{label[0].toUpperCase() + label.substring(1).toLowerCase()}</label></legend>
+            <input required={required} autoFocus={autofocus} type={format} id={label} name={label} />
         </fieldset>
     );
 }
