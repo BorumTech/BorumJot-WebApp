b2e1298867ddafc31f1ad5359346cf6d7ec3a60f
Varun <31084854+Borumer@users.noreply.github.com>
2020-11-15 18:38:05 -0500

    Move platform button list to footer

--- components/Layout/layout.js
+++ components/Layout/layout.js
@@ -1,26 +1,34 @@
-import layout from './layout.module.css';
-import Head from 'next/head';
+import layout from "./layout.module.css";
+import Head from "next/head";
+import PlatformButton from "../PlatformButton/platformButton";
 
 export default function Layout(props) {
-    return (
-        <div className={layout.container}>
-            <Head>
-                <link rel="icon" href="/images/favicon/favicon.ico" />
-            </Head>
+  return (
+    <div className={layout.container}>
+      <Head>
+        <link rel="icon" href="/images/favicon/favicon.ico" />
+      </Head>
 
-            {props.children}
-            
-            <footer className={layout.footer}>
-                <a
-                href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-                target="_blank"
-                rel="noopener noreferrer"
-                >
-                Powered by{' '}
-                <img src="/vercel.svg" alt="Vercel Logo" className={layout.logo} />
-                </a>
-                <p>&copy; 2020 Borum Tech</p>
-            </footer>
+      {props.children}
+
+      <footer className={layout.footer}>
+        <a
+          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Powered by{" "}
+          <img src="/vercel.svg" alt="Vercel Logo" className={layout.logo} />
+        </a>
+        <p>&copy; 2020 Borum Tech</p>
+        <div className={layout.platformList}>
+            <PlatformButton
+                downloadLink="https://play.google.com/store/apps/details?id=com.boruminc.borumjot.android"
+                imgSrc="/images/platforms/android-icon.png"
+                label="Get for free on the Google Play Store"
+            />
         </div>
-    );
+      </footer>
+    </div>
+  );
 }
--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,6 @@ import Head from "next/head";
 import Link from "next/link";
 import Image from 'next/image';
 import home from "./home.module.css";
-import PlatformButton from "../components/PlatformButton/platformButton";
 import FormField from "../components/FormField/formField";
 import Layout from "../components/Layout/layout";
 import { useState } from "react";
@@ -74,13 +73,6 @@ export default function Home() {
               <a target="_blank">New to Borum? Create an Account</a>
             </Link>
           </div>
-        </div>
-        <div className={home.grid}>
-          <PlatformButton
-            downloadLink="https://play.google.com/store/apps/details?id=com.boruminc.borumjot.android"
-            imgSrc="/images/platforms/android-icon.png"
-            label="Get for free on the Google Play Store"
-          />
         </div>
       </main>
     </Layout>
