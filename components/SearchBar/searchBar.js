b182694a5935fa10e2a7ee12c40e38eb63fcd0ea
Varun <31084854+Borumer@users.noreply.github.com>
2020-11-17 20:20:58 -0500

    Implement SearchBar component

--- components/SearchBar/searchBar.js
+++ components/SearchBar/searchBar.js
@@ -1,3 +1,21 @@
+import searchBar from "./searchBar.module.css";
+
 export default function SearchBar() {
-    
+  return (
+    <div className={searchBar.container}>
+      <button className={searchBar.searchBarIcon}>
+        <img
+          className={searchBar.greyCircle}
+          src="http://cdn.bforborum.com/images/search-bar-icon.png"
+          id="search-bar-icon-img"
+        />
+      </button>
+      <input
+        className={searchBar.searchBar}
+        onkeyup="searchFlytrap(this.value)"
+        type="text"
+        placeholder="Search your Jottings"
+      />
+    </div>
+  );
 }
