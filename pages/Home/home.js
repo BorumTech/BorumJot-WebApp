ed0d0059def74ea30718ec17d524bc078bbbf0e6
Varun <31084854+Borumer@users.noreply.github.com>
2020-11-22 15:31:56 -0500

    Implement account banner in home * Display Settings Link and Log Out button when account profile clicked * Account profile div shows generic profile icon (permanent), first name, last name, and down arrow or up arrow img (stored in state) * Fix bug with layout - Align to top so loading of jottings doesn't change 1st row's position * Add attributions.js page - required by flaticon for using arrow images * Use .prettierrc to make files tabs (4 spaces) instead of 2 spaces

--- pages/Home/home.js
+++ pages/Home/home.js
@@ -6,109 +6,122 @@ import TaskList from "../../components/JottingList/taskList";
 import { useState, useEffect } from "react";
 import ProgressSpinner from "../../components/CircularProgress/circularProgress";
 import { CONTENT_STATE } from "../../lib/view";
+import Image from "next/image";
+import Link from "next/link";
 
 export default function Home({ fade, onFadeInLogin, setFade }) {
-  const [notes, setNotes] = useState(null);
-  const [tasks, setTasks] = useState(null);
+	const [notes, setNotes] = useState(null);
+	const [tasks, setTasks] = useState(null);
 
-  useEffect(() => {
-    getJottings().then((response) => {
-      setNotes(response.notes);
-      setTasks(response.tasks);
-    });
-  }, []);
+	useEffect(() => {
+		getJottings().then((response) => {
+			setNotes(response.notes);
+			setTasks(response.tasks);
+		});
+	}, []);
 
-  const transitionToLogin = () => {
-    onFadeInLogin();
-    setFade(CONTENT_STATE.INVISIBLE);
-  };
+	const transitionToLogin = () => {
+		onFadeInLogin();
+		setFade(CONTENT_STATE.INVISIBLE);
+	};
 
-  const handleOnAnimationEnd = (e) => {
-    if (fade == CONTENT_STATE.FADE_OUT) {
-      transitionToLogin();
-    } else if (fade == CONTENT_STATE.FADE_IN) {
-      setFade(CONTENT_STATE.VISIBLE);
-    }
-  };
+	const handleOnAnimationEnd = (e) => {
+		if (fade == CONTENT_STATE.FADE_OUT) {
+			transitionToLogin();
+		} else if (fade == CONTENT_STATE.FADE_IN) {
+			setFade(CONTENT_STATE.VISIBLE);
+		}
+	};
 
-  return (
-    <main
-      onAnimationEnd={handleOnAnimationEnd}
-      className={
-        fade == CONTENT_STATE.INVISIBLE ? fade : `${fade} ${home.main}`
-      }
-    >
-      <BrandHeader />
-      <SearchBar />
-      <div className={home.ownNoteList}>
-        {notes ? <NoteList notes={notes} /> : <ProgressSpinner />}
-      </div>
-      <div className={home.ownTaskList}>
-        {tasks ? <TaskList tasks={tasks} /> : <ProgressSpinner />}
-      </div>
-      <AccountBanner />
-    </main>
-  );
+	return (
+		<main
+			onAnimationEnd={handleOnAnimationEnd}
+			className={
+				fade == CONTENT_STATE.INVISIBLE ? fade : `${fade} ${home.main}`
+			}
+		>
+			<BrandHeader />
+			<SearchBar />
+			<div className={home.ownNoteList}>
+				{notes ? <NoteList notes={notes} /> : <ProgressSpinner />}
+			</div>
+			<div className={home.ownTaskList}>
+				{tasks ? <TaskList tasks={tasks} /> : <ProgressSpinner />}
+			</div>
+			<AccountBanner setFade={setFade} />
+		</main>
+	);
 }
 
 function BrandHeader() {
-  return (
-    <div className="brandNameContainer">
-      <a href="/">
-        <LogoImage />
-        <span>Borum Jot</span>
-      </a>
-    </div>
-  );
+	return (
+		<div className="brandNameContainer">
+			<a href="/">
+				<LogoImage />
+				<span>Borum Jot</span>
+			</a>
+		</div>
+	);
 }
 
-function AccountBanner() {
-  const [accountMenuClass, setAccountMenuClass] = useState("hidden");
+function AccountBanner({ setFade }) {
+	const [accountMenuClass, setAccountMenuClass] = useState("hidden");
+	const [dropdownSrc, setDropdownSrc] = useState("down");
 
-  const handleLogOut = (e) => {
-    localStorage.removeItem("userApiKey");
-    setFade(CONTENT_STATE.FADE_OUT);
-  };
+	const handleLogOut = (e) => {
+		localStorage.removeItem("userApiKey");
+		setFade(CONTENT_STATE.FADE_OUT);
+	};
 
-  const openAccountMenu = e => {
-    setAccountMenuClass(accountMenuClass == "hidden" ? home.accountMenu : "hidden");
-  };
+	const openAccountMenu = (e) => {
+		setAccountMenuClass(
+			accountMenuClass == "hidden" ? home.accountMenu : "hidden"
+		);
+		setDropdownSrc(
+			dropdownSrc == "down" ? "up" : "down"
+		);
+	};
 
-  const firstName = window ? localStorage.getItem("firstName") : "";
-  const lastName = window ? localStorage.getItem("lastName") : "";
+	const firstName = window ? localStorage.getItem("firstName") : "";
+	const lastName = window ? localStorage.getItem("lastName") : "";
 
-  return (
-    <div className={home.accountBanner}>
-      <div className={home.account}>
-        <button className={home.profileBtn} onClick={openAccountMenu}>{`${firstName} ${lastName}`}</button>
-      </div>
-      <div className={accountMenuClass}>
-          <button className={home.logOut} onClick={handleLogOut}>Log Out</button>
-      </div>
-    </div>
-  );
+	return (
+		<div className={home.accountBanner}>
+			<button className={home.accountProfile} onClick={openAccountMenu}>
+				<Image width={28} height={28} src="/images/profile.png" />
+				<span>{`${firstName} ${lastName}`}</span>
+				<div className={home.dropdownArrow}>
+					<img width={16} height={16} src={`/images/arrow-${dropdownSrc}.png`} />
+				</div>
+			</button>
+			<ul className={accountMenuClass}>
+				<li><Link href="/Settings"><a className={home.settings}>Settings</a></Link></li>
+				<li><button className={home.logOut} onClick={handleLogOut}>Log Out</button></li>
+			</ul>
+		</div>
+	);
 }
 
 async function getJottings() {
-  const userApiKey = window.localStorage.getItem("userApiKey");
+	const userApiKey = window.localStorage.getItem("userApiKey");
 
-  const jottings = await fetch(
-    "https://api.jot.bforborum.com/api/v1/jottings",
-    {
-      method: "GET",
-      headers: {
-        authorization: "Basic " + userApiKey,
-        "content-type": "text/plain",
-      },
-    }
-  );
+	const jottings = await fetch(
+		"https://api.jot.bforborum.com/api/v1/jottings",
+		{
+			method: "GET",
+			headers: {
+				authorization: "Basic " + userApiKey,
+				"content-type": "text/plain",
+			},
+		}
+	);
 
-  if (jottings.status == 200) {
-    let { data } = await jottings.json();
+	if (jottings.status == 200) {
+		let { data } = await jottings.json();
 
-    return {
-      notes: data.filter((item) => item.source == "note"),
-      tasks: data.filter((item) => item.source == "task"),
-    };
-  }
+		return {
+			notes: data.filter((item) => item.source == "note"),
+			tasks: data.filter((item) => item.source == "task"),
+		};
+	}
 }
