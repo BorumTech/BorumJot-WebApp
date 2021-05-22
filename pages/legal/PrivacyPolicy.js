a865a9965efa283e5e7c9bb4c78a43b42801b124
Varun <31084854+Borumer@users.noreply.github.com>
2021-05-22 16:26:11 -0400

    Implement showDocumentOnly for privacy policy * Take in optional showDocumentOnly query parameter on PrivacyPolicy page to only show PrivacyPolicy and nothing else * Take in optional hideHeading query parameter that controls whether to hide "Borum Jot Privacy Policy" and just show the document body

--- pages/legal/PrivacyPolicy.js
+++ pages/legal/PrivacyPolicy.js
@@ -3,35 +3,59 @@ import Layout from "../../components/Layout/layout";
 import BrandHeader from "../../components/BrandHeader/brandHeader";
 import { getPrivacyPolicyContent } from "../../libs/legal";
 import privacyPolicy from "./privacyPolicy.module.css";
+import { useRouter } from "next/router";
 
 export default function PrivacyPolicy(props) {
+	const router = useRouter();
+
+	const commonHead = (
+		<Head>
+			<link
+				href="https://cdn.jsdelivr.net/gh/Borumer/Flytrap@1cca457/css/sub.css"
+				rel="stylesheet"
+				type="text/css"
+			/>
+			<link
+				href="https://cdn.jsdelivr.net/gh/Borumer/Flytrap@1cca457/legal/legal.css"
+				rel="stylesheet"
+				type="text/css"
+			/>
+			<link
+				href="https://fonts.googleapis.com/css2?family=Odibee+Sans&display=swap"
+				rel="stylesheet"
+			/>
+
+			<title>Borum Jot Privacy Policy</title>
+		</Head>
+	);
+
+	if (router.query.showDocumentOnly) {
+		return (
+			<div className={privacyPolicy.documentOnlyContainer}>
+				{commonHead}
+				<Head>
+					<link rel="icon" href="/images/favicon/favicon.ico" />
+				</Head>
+				{!!router.query.hideHeading ? "" : <h1>Borum Jot Privacy Policy</h1>}
+				<article
+					className={privacyPolicy.legalDocumentContent}
+					dangerouslySetInnerHTML={{ __html: props.content }}
+				/>
+			</div>
+		);
+	}
+
 	return (
 		<Layout>
-			<Head>
-				<link
-					href="https://cdn.jsdelivr.net/gh/Borumer/Flytrap@1cca457/css/sub.css"
-					rel="stylesheet"
-					type="text/css"
-				/>
-				<link
-					href="https://cdn.jsdelivr.net/gh/Borumer/Flytrap@1cca457/legal/legal.css"
-					rel="stylesheet"
-					type="text/css"
-				/>
-				<link
-					href="https://fonts.googleapis.com/css2?family=Odibee+Sans&display=swap"
-					rel="stylesheet"
-				/>
-				<title>Borum Jot Privacy Policy</title>
-			</Head>
+			{commonHead}
 
 			<main>
 				<BrandHeader />
 
-				<div className={privacyPolicy.grid}>
-					<h1>Privacy Policy</h1>
+				<div className={privacyPolicy.layoutFilledContainer}>
+					<h1>Borum Jot Privacy Policy</h1>
 					<article
-						className="legal-document-content"
+						className={privacyPolicy.legalDocumentContent}
 						dangerouslySetInnerHTML={{ __html: props.content }}
 					/>
 				</div>
