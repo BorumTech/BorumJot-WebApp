11f795c2b0f50f50bf6c487d5b2bc0a6e5fe1440
Varun <31084854+Borumer@users.noreply.github.com>
2021-05-22 17:04:24 -0400

    Cut to one style for android privacy policy

--- pages/legal/PrivacyPolicy/androidApp.js
+++ pages/legal/PrivacyPolicy/androidApp.js
@@ -1,21 +1,16 @@
-import {
-	legalDocumentContent,
-	documentOnlyContainer,
-} from "../privacyPolicy.module.css";
 import CommonHead from "../commonHead";
 import Head from "next/head";
 import { getPrivacyPolicyContent } from "../../../libs/legal";
 
 export default function AndroidApp(props) {
 	return (
-		<div className={documentOnlyContainer}>
+		<div style={{padding: "1em"}}>
 			<CommonHead />
 			<Head>
 				<link rel="icon" href="/images/favicon/favicon.ico" />
 			</Head>
 
 			<article
-				className={legalDocumentContent}
 				dangerouslySetInnerHTML={{ __html: props.content }}
 			/>
 		</div>
